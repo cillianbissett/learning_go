c32e234f777bac79bd64c9de95aa931798d71e4e
unknown <cillbis@gmail.com>
2024-02-24 11:37:14 +0000

    Deliberately introducing bug to test SonarCloud

--- tutorials/tutorial_6_goroutines/main.go
+++ tutorials/tutorial_6_goroutines/main.go
@@ -7,9 +7,10 @@ import (
 	"time"
 )
 
-//NB Concurrency != Parallelism, difference is flicking back and forth vs same time
+// NB Concurrency != Parallelism, difference is flicking back and forth vs same time
 // can get parallel execution in some cases, but not always
-
+// there is also read/write locks, channels, and other ways to manage concurrency
+var m = sync.Mutex{}
 var wg = sync.WaitGroup{}
 var dbData = []string{"id1", "id2", "id3", "id4", "id5", "id6", "id7", "id8", "id9", "id10"}
 var results = []string{}
@@ -29,6 +30,9 @@ func dbCall(i int) {
 	var delay float32 = rand.Float32() * 2000
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 	fmt.Println("Result from DB: ", dbData[i])
+	// need to apply a lock to stop concurrent shenanigans
+	m.Lock()
 	results = append(results, dbData[i])
+	//get rid of lock to allow next guy in line to append
 	wg.Done()
 }
